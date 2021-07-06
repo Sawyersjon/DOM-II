d080c882cfe0f2ed221e44e513c644528d0e5064
Jon Sawyers <tg9569pb@gmail.com>
2021-07-06 14:21:11 -0500

    added two events to header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+let header = document.querySelector('.container.nav-container');
+
+header.addEventListener('mouseenter', (event) => {
+    event.target.style.backgroundColor = 'red';
+header.addEventListener('mouseleave', (event) =>{
+    event.target.style.backgroundColor = 'white';
+})
+});
+
+let anchor = document.querySelector('.nav-link');
+
+anchor.addEventListener('click', (event) => {
+    event.onclick.style.fontWeight = 'bold';
+})
